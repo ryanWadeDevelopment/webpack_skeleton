804e1e0fb695384bd6c018b50eef253a7e5be63b
ryanWadeDevelopment <62427162+ryanWadeDevelopment@users.noreply.github.com>
2022-02-01 17:42:50 -0600

    Add files via upload

---
+++ webpack_skeleton/webpack.common.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs',
+          },
+        },
+      },
+    ],
+  },
+  plugins: [],
+};
